0bb1cbe97ea23251c368b737689b098e25dea2a4
Konstantin Babeshin <connect@babeshin.space>
2022-11-01 11:30:05 +0500

    Без лишних выводов

--- index.php
+++ index.php
@@ -8,12 +8,6 @@ $connect = BD::GetCon('localhost','root','root','prodamus');
 $q = 'orders';
 $sql_array = BD::SelectAll($connect,$q);
 
-
-
-// 
-echo '<pre>' . print_r($sql_array, 1) . '</pre>';
-echo '<pre>' . print_r($csv_array, 1) . '</pre>';
-
 $orders = array();
 foreach($csv_array as $key=>$value){
     if(array_key_exists($key,$sql_array)){
@@ -21,5 +15,4 @@ foreach($csv_array as $key=>$value){
         array_push($orders[$key],$csv_array[$key]);
     }
 }
-echo '<pre>' . print_r($orders, 1) . '</pre>';
-// 
+ 
